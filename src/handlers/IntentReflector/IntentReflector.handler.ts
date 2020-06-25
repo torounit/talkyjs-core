6963411032163aa1c2c6a3630c29a1bb9ee283d7
Hidetaka Okamoto <info@wp-kyoto.net>
2020-06-25 22:56:18 +0900

    chore: move files

---
+++ src/handlers/IntentReflector/IntentReflector.handler.ts
@@ -0,0 +1,18 @@
+import { RequestHandler, getRequest } from 'ask-sdk-core';
+import { LoggerService } from '../../Logger';
+import {IntentRequest } from 'ask-sdk-model'
+
+export const IntentRelectorHandler: RequestHandler = {
+    canHandle (input) {
+        return input.requestEnvelope.request.type === 'IntentRequest'
+    },
+    handle (input) {
+        const { logger } = LoggerService.getInstance()
+        logger.info('IntentReflector was called')
+        const intentName = getRequest<IntentRequest>(input.requestEnvelope).intent.name
+        const speechText = `You just triggered ${intentName}`
+        return input.responseBuilder
+            .speak(speechText)
+            .getResponse()
+    }
+}
